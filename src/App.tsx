0828e079e260a859dae1d9d2ad11437c220135d9
Lucas Felipe <lucasfelipemk@gmail.com>
2024-01-24 23:08:02 -0300

    :wrench: feat: Base config for React Router Dom working in the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 export function App() {
   return (
     <main className="bg-gray-900 text-gray-300 h-screen w-screen">
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </main>
   )
 }
