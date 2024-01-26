7085c9ed7fa492ca362cfe3b7ad25f7677783a19
Lucas Felipe <lucasfelipemk@gmail.com>
2024-01-25 23:29:22 -0300

    :sparkles: feat: Created a Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,29 @@
+import { History, Timer } from 'lucide-react'
+import igniteLogo from '../assets/igniteLogo.svg'
+import { NavLink } from 'react-router-dom'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <header className="flex items-center justify-between">
+      <img src={igniteLogo} alt="" />
+
+      <nav className="flex items-center justify-center gap-3 text-gray-500 ">
+        <NavLink
+          to="/"
+          title="Timer"
+          className="w-12 h-12 flex items-center justify-center border-b-[3px] border-t-[3px] border-transparent rounded-t hover:border-b-green-500 hover:bg-zinc-800 transition-colors duration-150 aria-[current='page']:border-b-green-500 aria-[current='page']:text-green-500"
+        >
+          <Timer size={28} />
+        </NavLink>
+
+        <NavLink
+          to="/history"
+          title="Histórico"
+          className="w-12 h-12 flex items-center justify-center border-b-[3px] border-t-[3px] border-transparent rounded-t hover:border-b-green-500 hover:bg-zinc-800 transition-colors duration-150 aria-[current='page']:border-b-green-500 aria-[current='page']:text-green-500"
+        >
+          <History size={28} />
+        </NavLink>
+      </nav>
+    </header>
+  )
 }
