561fc3507d02e7867c7d6cf131be2584cc24a4f1
Lucas Felipe <lucasfelipemk@gmail.com>
2024-01-25 23:11:43 -0300

    :bug: fix: Added 1px of padding top for fix margin bug

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Router } from './Router'
 
 export function App() {
   return (
-    <main className="bg-gray-900 text-gray-300 h-screen w-screen">
+    <main className="h-screen w-screen pt-[1px] bg-gray-900 text-gray-300">
       <BrowserRouter>
         <Router />
       </BrowserRouter>
