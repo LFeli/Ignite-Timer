ac30b015c0b43ef292aeeae1a202b28b4896e7ef
Lucas Felipe <lucasfelipemk@gmail.com>
2024-01-27 17:33:17 -0300

    :sparkles: feat: Added base structure and style to history page

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,3 +1,87 @@
 export function History() {
-  return <h1>History</h1>
+  return (
+    <main className="flex-1 p-14 flex flex-col">
+      <h1 className="text-2xl text-gray-100 font-bold">Meu histórico</h1>
+
+      <div className="flex-1 overflow-auto mt-10">
+        <table className="w-full border-collapse min-w-[600px]">
+          <thead>
+            <tr>
+              <th className="bg-gray-600 p-4 text-left text-gray-100 text-sm leading-relaxed rounded-tl-lg pl-6">
+                Tarefas
+              </th>
+              <th className="bg-gray-600 p-4 text-left text-gray-100 text-sm leading-relaxed">
+                Duração
+              </th>
+              <th className="bg-gray-600 p-4 text-left text-gray-100 text-sm leading-relaxed">
+                Início
+              </th>
+              <th className="bg-gray-600 p-4 text-left text-gray-100 text-sm leading-relaxed rounded-tr-lg pr-6">
+                Status
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed w-1/2 pl-6">
+                Tarefa 01
+              </td>
+              <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed">
+                20 Minutos
+              </td>
+              <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed">
+                Há cerca de 2 minutos atrás
+              </td>
+              <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed pr-6">
+                Concluído
+              </td>
+            </tr>
+            <tr>
+              <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed w-1/2 pl-6">
+                Tarefa 01
+              </td>
+              <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed">
+                20 Minutos
+              </td>
+              <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed">
+                Há cerca de 2 minutos atrás
+              </td>
+              <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed pr-6">
+                Concluído
+              </td>
+            </tr>
+            <tr>
+              <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed w-1/2 pl-6">
+                Tarefa 01
+              </td>
+              <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed">
+                20 Minutos
+              </td>
+              <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed">
+                Há cerca de 2 minutos atrás
+              </td>
+              <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed pr-6">
+                Concluído
+              </td>
+            </tr>
+            <tr>
+              <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed w-1/2 pl-6">
+                Tarefa 01
+              </td>
+              <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed">
+                20 Minutos
+              </td>
+              <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed">
+                Há cerca de 2 minutos atrás
+              </td>
+              <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed pr-6">
+                Concluído
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+  )
 }
