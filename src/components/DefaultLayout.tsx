aa52fd9a81fcf3be471d462c5dd7e7c5d69f1a2d
Lucas Felipe <lucasfelipemk@gmail.com>
2024-01-28 17:59:14 -0300

    :bug: fix: Added missing padding in tablet or larger in 'DefaultLayout.tsx'

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -3,7 +3,7 @@ import { Header } from '../components/Header'
 
 export function DefaultLayout() {
   return (
-    <div className="bg-gray-50 dark:bg-gray-800 w-full h-LayoutContainerMobile md:h-LayoutContainerTablet lg:h-LayoutContainer max-w-6xl p-4 rounded-lg flex flex-col">
+    <div className="bg-gray-50 dark:bg-gray-800 w-full h-LayoutContainerMobile md:h-LayoutContainerTablet lg:h-LayoutContainer max-w-6xl p-4 md:p-10 rounded-lg flex flex-col">
       <Header />
       <Outlet />
     </div>
