955fae890476f997dc9a94b4019c45cd81f8f395
Lucas Felipe <lucasfelipemk@gmail.com>
2024-01-27 23:22:02 -0300

    :recycle: refactor: Re-build Header component but usign some components of ShadcnUI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,52 +1,64 @@
-import { History, Timer } from 'lucide-react'
-import igniteLogo from '../assets/igniteLogo.svg'
+import { Timer, History } from 'lucide-react'
 import { NavLink } from 'react-router-dom'
-import { HoverCard, HoverCardContent, HoverCardTrigger } from './ui/hover-card'
+import IgniteLogo from '../assets/igniteLogo.svg'
+import { ModeToggle } from './ModeToggle'
+import {
+  HoverCard,
+  HoverCardArrow,
+  HoverCardContent,
+  HoverCardTrigger,
+} from './ui/hover-card'
 
 export function Header() {
   return (
-    <header className="flex items-center justify-between">
-      <img src={igniteLogo} alt="" />
+    <header>
+      <nav className="flex items-center justify-between">
+        <div className="flex items-center justify-center gap-8">
+          <img src={IgniteLogo} alt="" />
+          <ModeToggle />
+        </div>
 
-      <nav className="flex items-center justify-center gap-3 text-gray-500 ">
-        <HoverCard openDelay={600} closeDelay={150}>
-          <HoverCardTrigger>
-            {' '}
-            <NavLink
-              to="/"
-              title="Timer"
-              className="w-12 h-12 flex items-center justify-center border-b-[3px] border-t-[3px] border-transparent rounded-t hover:border-b-green-500 hover:bg-zinc-800 transition-colors duration-150 aria-[current='page']:border-b-green-500 aria-[current='page']:text-green-500"
+        <div className="flex items-center justify-center gap-3 text-gray-500">
+          <HoverCard openDelay={600} closeDelay={150}>
+            <HoverCardTrigger>
+              {' '}
+              <NavLink
+                to="/"
+                className="w-12 h-12 flex items-center justify-center border-b-[3px] border-t-[3px] border-transparent rounded-t hover:border-b-green-500 hover:bg-zinc-800 transition-colors duration-150 aria-[current='page']:border-b-green-500 aria-[current='page']:text-green-500"
+              >
+                <Timer size={28} />
+              </NavLink>
+            </HoverCardTrigger>
+            <HoverCardContent
+              side="top"
+              sideOffset={15}
+              className="bg-zinc-950 text-gray-200 text border-none"
             >
-              <Timer size={28} />
-            </NavLink>
-          </HoverCardTrigger>
-          <HoverCardContent
-            side="top"
-            sideOffset={15}
-            className="bg-gray-700 text-gray-200 text border-none"
-          >
-            Timer
-          </HoverCardContent>
-        </HoverCard>
+              Timer
+              <HoverCardArrow className="fill-zinc-950" />
+            </HoverCardContent>
+          </HoverCard>
 
-        <HoverCard openDelay={600} closeDelay={150}>
-          <HoverCardTrigger>
-            <NavLink
-              to="/history"
-              title="Histórico"
-              className="w-12 h-12 flex items-center justify-center border-b-[3px] border-t-[3px] border-transparent rounded-t hover:border-b-green-500 hover:bg-zinc-800 transition-colors duration-150 aria-[current='page']:border-b-green-500 aria-[current='page']:text-green-500"
+          <HoverCard openDelay={600} closeDelay={150}>
+            <HoverCardTrigger>
+              {' '}
+              <NavLink
+                to="/history"
+                className="w-12 h-12 flex items-center justify-center border-b-[3px] border-t-[3px] border-transparent rounded-t hover:border-b-green-500 hover:bg-zinc-800 transition-colors duration-150 aria-[current='page']:border-b-green-500 aria-[current='page']:text-green-500"
+              >
+                <History size={28} />
+              </NavLink>
+            </HoverCardTrigger>
+            <HoverCardContent
+              side="top"
+              sideOffset={15}
+              className="bg-zinc-950 text-gray-200 text border-none"
             >
-              <History size={28} />
-            </NavLink>
-          </HoverCardTrigger>
-          <HoverCardContent
-            side="top"
-            sideOffset={15}
-            className="bg-gray-700 text-gray-200 text border-none"
-          >
-            History
-          </HoverCardContent>
-        </HoverCard>
+              Histórico
+              <HoverCardArrow className="fill-zinc-950" />
+            </HoverCardContent>
+          </HoverCard>
+        </div>
       </nav>
     </header>
   )
