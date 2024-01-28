ab5bb6e7da3a081efe70c80da5fbee6f24582b54
Lucas Felipe <lucasfelipemk@gmail.com>
2024-01-27 22:40:20 -0300

    :sparkles: feat: Created a theme provider and toggle mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
+import { ThemeProvider } from './components/theme-provider'
 
 export function App() {
   return (
-    <main className="h-screen w-full px-4 md:px-8 rounded-lg bg-gray-900 text-gray-300 flex items-center justify-center">
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </main>
+    <ThemeProvider>
+      <main className="h-screen w-full px-4 md:px-8 rounded-lg bg-gray-900 text-gray-300 flex items-center justify-center">
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </main>
+    </ThemeProvider>
   )
 }
