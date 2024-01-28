1ef80c8f8f82fb847b5af5642dd5dd159184d45d
Lucas Felipe <lucasfelipemk@gmail.com>
2024-01-28 14:23:33 -0300

    :truck: chore: Moving 'DefaultLayout.tsx' to inside the folder src

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -1,11 +1,11 @@
-// import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { Header } from '../components/Header'
 
 export function DefaultLayout() {
   return (
     <div className="bg-gray-50 dark:bg-gray-800 w-full h-LayoutContainerMobile md:h-LayoutContainerTablet lg:h-LayoutContainer max-w-6xl p-4 rounded-lg flex flex-col">
       <Header />
-      {/* <Outlet /> */}
+      <Outlet />
     </div>
   )
 }
