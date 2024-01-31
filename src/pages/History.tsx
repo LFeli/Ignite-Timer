6b77ebc949698fae9a9cac38469c1de0839e1306
Lucas Felipe <lucasfelipemk@gmail.com>
2024-01-30 22:35:57 -0300

    :recycle: refactor: Added table usign component by ShadcnUI and turn responsive

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,13 +1,163 @@
-import { Table as HistoryTable } from '@/components/Table'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 
 export function History() {
   return (
-    <main className="bg-red-500 flex flex-1 flex-col gap-8">
+    <main className="flex flex-1 flex-col gap-8">
       <h1 className="text-lg md:text-2xl dark:text-gray-100 font-bold pt-10">
         Meu histórico
       </h1>
 
-      <HistoryTable />
+      <Table className="overflow-hidden flex-1">
+        <TableHeader className="">
+          <TableRow className="bg-gray-200 dark:bg-gray-600 hover:bg-gray-200 dark:hover:bg-gray-600">
+            <TableHead>Tarefas</TableHead>
+            <TableHead>Duração</TableHead>
+            <TableHead>Início</TableHead>
+            <TableHead>Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </main>
   )
 }
