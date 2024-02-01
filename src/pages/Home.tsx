d4f3f11bb628fed1e88fc8357554c3638a7fb2ec
Lucas Felipe <lucasfelipemk@gmail.com>
2024-02-01 20:42:40 -0300

    :wrench: feat: Implement a simple usign of react-hook-form lib

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,32 @@
 import { Button } from '@/components/ui/button'
 import { Play } from 'lucide-react'
+import { useForm } from 'react-hook-form'
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm()
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
+  function handleCreateNewCycle(data) {
+    console.log(data)
+  }
+
   return (
     <main className=" flex flex-col flex-1 items-center justify-center">
       <div className="w-full max-w-2xl">
-        <form className="flex flex-col items-center gap-14">
+        <form
+          onSubmit={handleSubmit(handleCreateNewCycle)}
+          className="flex flex-col items-center gap-14"
+        >
           <div className="w-full flex flex-col md:flex-row gap-10 md:gap-6 font-bold md:text-lg">
             <div className="flex md:flex-1 gap-2">
               <label htmlFor="task">Vou trabalhar em</label>
               <input
                 type="text"
-                id="tasks"
+                id="task"
                 placeholder="Dê um nome ao seu projeto"
+                {...register('task')}
                 className=" flex-1 w-full px-2 pb-2 bg-transparent text-gray-500 dark:text-gray-100 border-b-2 border-b-gray-500 placeholder:text-gray-500 focus:border-b-green-500 focus:outline-none"
               />
             </div>
@@ -26,6 +40,7 @@ export function Home() {
                 max={60}
                 id="minutesAmount"
                 placeholder="00"
+                {...register('minutesAmount')}
                 className="px-2 pb-2 bg-transparent text-gray-500 dark:text-gray-100 border-b-2 border-b-gray-500 placeholder:text-gray-500 focus:border-b-green-500 focus:outline-none"
               />
 
@@ -57,7 +72,10 @@ export function Home() {
             </div>
           </div>
 
-          <Button className="w-full flex-1 py-3 md:py-4 bg-green-500 hover:bg-green-700 disabled:bg-green-500 text-gray-100">
+          <Button
+            className="w-full flex-1 py-3 md:py-4 bg-green-500 hover:bg-green-700 disabled:bg-green-500 text-gray-100"
+            disabled={isSubmitDisabled}
+          >
             <Play size={24} />
             Começar
           </Button>
