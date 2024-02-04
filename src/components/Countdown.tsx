2ddcdc0ca46f1b5be87481702fe9f449b2d78713
Lucas Felipe <lucasfelipemk@gmail.com>
2024-02-03 21:47:20 -0300

    :sparkles: feat: Converting Coutdown component for usign context

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,8 +1,28 @@
+import { CyclesContext } from '@/pages/Home'
 import { differenceInSeconds } from 'date-fns'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 export function Countdown() {
+  const { activeCycle, activeCycleID, markCurrentCycleAsFinished } =
+    useContext(CyclesContext)
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    } else {
+      document.title = 'Ignite Timer'
+    }
+  }, [minutes, seconds, activeCycle])
 
   useEffect(() => {
     let interval: number | NodeJS.Timeout
@@ -15,16 +35,7 @@ export function Countdown() {
         )
 
         if (secondsDifference >= totalSeconds) {
-          setCycles((state) =>
-            state.map((cycle) => {
-              if (cycle.id === activeCycleID) {
-                return { ...cycle, finishedDate: new Date() }
-              } else {
-                return cycle
-              }
-            }),
-          )
-
+          markCurrentCycleAsFinished()
           setAmountSecondsPassed(totalSeconds)
 
           clearInterval(interval)
@@ -37,7 +48,7 @@ export function Countdown() {
     return () => {
       clearInterval(interval)
     }
-  }, [activeCycle, totalSeconds, activeCycleID])
+  }, [activeCycle, totalSeconds, activeCycleID, markCurrentCycleAsFinished])
 
   return (
     <div className="w-full flex items-center justify-between font-robotoMono text-4xl 2mb:text-8xl md:text-[8rem] leading-relaxed  text-gray-600 dark:text-gray-100 select-none">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,8 @@
 import { Button } from '@/components/ui/button'
 import { Play, X } from 'lucide-react'
-import { useEffect, useState } from 'react'
-import { differenceInSeconds } from 'date-fns'
+import { createContext, useState } from 'react'
 import { CycleForm } from '@/components/CycleForm'
 import { Countdown } from '@/components/Countdown'
-// import { watch } from 'fs'
 
 interface Cycle {
   id: string
@@ -15,78 +13,84 @@ interface Cycle {
   finishedDate?: Date
 }
 
+interface CyclesContextType {
+  activeCycle: Cycle | undefined
+  activeCycleID: string | null
+  markCurrentCycleAsFinished: () => void
+}
+
+export const CyclesContext = createContext({} as CyclesContextType)
+
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([])
   const [activeCycleID, setActiveCycleID] = useState<string | null>(null)
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleID)
 
-  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
-
-  function handleCreateNewCycle(data: NewCycleFormData) {
-    const id = String(new Date().getTime())
-    const newCycle: Cycle = {
-      id,
-      task: data.task,
-      minutesAmount: data.minutesAmount,
-      startDate: new Date(),
-    }
-
-    setCycles((state) => [...state, newCycle])
-    setActiveCycleID(id)
-    setAmountSecondsPassed(0)
-
-    reset()
-  }
-
-  function handleInterruptCycle() {
+  function markCurrentCycleAsFinished() {
     setCycles((state) =>
       state.map((cycle) => {
         if (cycle.id === activeCycleID) {
-          return { ...cycle, interruptedDate: new Date() }
+          return { ...cycle, finishedDate: new Date() }
         } else {
           return cycle
         }
       }),
     )
-
-    setActiveCycleID(null)
   }
 
-  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+  // function handleCreateNewCycle(data: NewCycleFormData) {
+  //   const id = String(new Date().getTime())
+  //   const newCycle: Cycle = {
+  //     id,
+  //     task: data.task,
+  //     minutesAmount: data.minutesAmount,
+  //     startDate: new Date(),
+  //   }
 
-  const minutesAmount = Math.floor(currentSeconds / 60)
-  const secondsAmount = currentSeconds % 60
+  //   setCycles((state) => [...state, newCycle])
+  //   setActiveCycleID(id)
+  //   setAmountSecondsPassed(0)
 
-  const minutes = String(minutesAmount).padStart(2, '0')
-  const seconds = String(secondsAmount).padStart(2, '0')
+  //   reset()
+  // }
 
-  useEffect(() => {
-    if (activeCycle) {
-      document.title = `${minutes}:${seconds}`
-    } else {
-      document.title = 'Ignite Timer'
-    }
-  }, [minutes, seconds, activeCycle])
+  // function handleInterruptCycle() {
+  //   setCycles((state) =>
+  //     state.map((cycle) => {
+  //       if (cycle.id === activeCycleID) {
+  //         return { ...cycle, interruptedDate: new Date() }
+  //       } else {
+  //         return cycle
+  //       }
+  //     }),
+  //   )
 
-  const task = watch('task')
-  const isSubmitDisabled = !task
+  //   setActiveCycleID(null)
+  // }
+
+  // const task = watch('task')
+  // const isSubmitDisabled = !task
 
   return (
     <main className=" flex flex-col flex-1 items-center justify-center">
       <div className="w-full max-w-2xl">
         <form
-          onSubmit={handleSubmit(handleCreateNewCycle)}
+          // onSubmit={handleSubmit(handleCreateNewCycle)}
           className="flex flex-col items-center gap-14"
         >
-          <CycleForm />
-          <Countdown />
+          <CyclesContext.Provider
+            value={{ activeCycle, activeCycleID, markCurrentCycleAsFinished }}
+          >
+            {/* <CycleForm /> */}
+            <Countdown />
+          </CyclesContext.Provider>
 
           {activeCycle ? (
             <Button
               className="w-full flex-1 gap-2 py-3 md:py-4 bg-red-500 hover:bg-red-700 text-gray-100"
               type="button"
-              onClick={handleInterruptCycle}
+              // onClick={handleInterruptCycle}
             >
               <X size={24} />
               Interromper
@@ -95,7 +99,7 @@ export function Home() {
             <Button
               className="w-full flex-1 gap-1 py-3 md:py-4 bg-green-500 hover:bg-green-700 disabled:bg-green-500 text-gray-100"
               type="submit"
-              disabled={isSubmitDisabled}
+              // disabled={isSubmitDisabled}
             >
               <Play size={24} />
               Começar
